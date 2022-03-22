898404b9a6e49d621ba062ede81bf2eadd872d2a
Utkarsh <utkarsh.psrivastava@cliksource.com>
2022-03-22 20:45:22 +0530

    Added Picasoo for fetching image

--- app/src/main/java/com/utkarsh/spokesly/ui/adapter/PostPhotosListAdapter.kt
+++ app/src/main/java/com/utkarsh/spokesly/ui/adapter/PostPhotosListAdapter.kt
@@ -1,18 +1,15 @@
 package com.utkarsh.spokesly.ui.adapter
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
+import com.squareup.picasso.Picasso
 import com.utkarsh.spokesly.R
-import com.utkarsh.spokesly.data.models.PostCommentsResponse
 import com.utkarsh.spokesly.data.models.PostImageResponse
-import com.utkarsh.spokesly.data.models.UserResponse
 
 
 class PostPhotosListAdapter(
@@ -46,12 +43,9 @@ class PostPhotosListAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = photoList?.get(position)
-        Log.e("Photo Url","${item.thumbnailUrl}")
-        Glide.with(argContext)
-            .load(item.thumbnailUrl)
-            .placeholder(R.drawable.ic_image_preview) //When there is No Image
-            .error(R.drawable.ic_image_preview) // When Some URL fails to Load.
-            .into(holder.image)
+        var trimImage = item.thumbnailUrl.trim()
+        val picasoo = Picasso.get()
+        picasoo.load(trimImage).into(holder.image);
         holder.title.text =  "Title : ${item.title}"
         holder.album_id.text = "Album id: ${item.albumId}"
         holder.id.text = "Id : ${item.id}"
